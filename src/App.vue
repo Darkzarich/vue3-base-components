fd41f1d9323bb0870c13b835d930e0d1b9d75a62
Darkzarich <darkzarichv2@gmail.com>
2021-12-20 19:02:24 +0300

    add base styles; css variables; BaseInput component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="components-list">
+    <div class="input-block">
+      <div class="input-block__input">
+        <BaseInput
+          v-model="formData.title"
+          label="Title"
+          type="text"
+          maxlength="20"
+        />
+      </div>
+    </div>
+  </div>
+  <div class="result">
+    <div class="result__page">
+      <div class="result__title">
+        {{ formData.title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { reactive } from "vue";
+import BaseInput from "@/components/BaseInput.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    BaseInput,
+  },
+  setup() {
+    const formData = reactive({
+      title: "",
+    });
+
+    return {
+      formData,
+    };
   },
 };
 </script>
 
 <style lang="scss">
+:root {
+  --color-white: #fff;
+  --color-border: #8c92ac;
+  --color-active: #a8b8ff;
+  --color-result-page: #f2ffff;
+  --color-text: #2c3e50;
+}
+
+body {
+  margin: 20px;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: var(--color-text);
   margin-top: 60px;
+  display: flex;
+}
+
+.result {
+  width: 50%;
+  border: 1px solid var(--color-border);
+  border-left: none;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__page {
+    width: 140px;
+    height: 190px;
+    border: 1px solid var(--color-border);
+    background: var(--color-result-page);
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  &__title {
+    font-size: 12px;
+    text-align: center;
+  }
+}
+
+.components-list {
+  width: 50%;
+  border: 1px solid var(--color-border);
+  padding: 20px;
 }
 </style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="input">
+    <label class="input__label">
+      {{ label }}
+    </label>
+    <input
+      class="input__field"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["update"],
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.input {
+  position: relative;
+
+  &__label {
+    position: absolute;
+    font-size: 13px;
+    background: var(--color-white);
+    top: -7px;
+    left: 10px;
+    color: var(--color-border);
+    padding: 0 3px;
+  }
+
+  &__field {
+    padding: 12px;
+    border-radius: 3px;
+    border: 1px solid var(--color-active);
+    color: var(--color-text);
+
+    &:focus,
+    &:hover {
+      border: 1px solid var(--color-active);
+      outline: none;
+    }
+
+    &:focus {
+      border-bottom: 2px solid var(--color-active);
+    }
+  }
+}
+</style>
