83160effdd221029fa6602732d4d0c8fa546cf22
Darkzarich <darkzarichv2@gmail.com>
2021-12-28 15:20:57 +0300

    add BaseRadio.vue tests

---
+++ tests/unit/BaseRadio.spec.js
@@ -0,0 +1,48 @@
+import { shallowMount } from "@vue/test-utils";
+import BaseRadio from "@/components/Base/BaseRadio.vue";
+
+describe("BaseRadio.vue", () => {
+  it("renders an radio button", () => {
+    const wrapper = shallowMount(BaseRadio, { props: { value: 1 } });
+
+    expect(wrapper.find('input[type="radio"]').exists()).toBe(true);
+  });
+
+  it("renders a :label of a checkbox", () => {
+    const label = "My label";
+    const wrapper = shallowMount(BaseRadio, {
+      props: {
+        label,
+        value: "_",
+      },
+    });
+
+    expect(wrapper.text()).toContain(label);
+  });
+
+  it("emits passed value on @update:modelValue", async () => {
+    const value = 1;
+    const wrapper = shallowMount(BaseRadio, {
+      props: {
+        value,
+      },
+    });
+    const radio = wrapper.find("input");
+
+    await radio.trigger("change");
+
+    expect(wrapper.emitted()["update:modelValue"][0][0]).toBe(value);
+  });
+
+  it("is checked if :modelValue and :value equal", async () => {
+    const value = "yes";
+    const wrapper = shallowMount(BaseRadio, {
+      props: {
+        modelValue: value,
+        value,
+      },
+    });
+
+    expect(wrapper.find("input").element.checked).toBeTruthy();
+  });
+});
